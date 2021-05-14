6b610bf88ad61d32d77c7a90fb46440019679a1a
irving1596 <irving.sanchezp@up.ac.pa>
2021-05-14 02:16:37 +0000

    se separa el login por rol

--- ElBanco/src/Login_main.java
+++ ElBanco/src/Login_main.java
@@ -31,24 +31,22 @@ public class Login_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//MarcoBaseBankATM bankATM = new MarcoBaseBankATM();
-		//bankATM.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		// MarcoBaseBankATM bankATM = new MarcoBaseBankATM();
+		// bankATM.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //		MarcoBaseBankATM();
 		MarcoAcceso loginMarco = new MarcoAcceso("DMG Bank");
 		loginMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	//generando un metodo estatico para crear el objeto de la clase MarcoAcceso
+	// generando un metodo estatico para crear el objeto de la clase MarcoAcceso
 //	private static void MarcoBaseBankATM(){
 //		MarcoAcceso loginMarco = new MarcoAcceso("Bank $.$");
 //		loginMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //	}
-	
-	
+
 }
 
-
 @SuppressWarnings("serial")
-class MarcoAcceso extends JFrame implements KeyListener, ActionListener{
+class MarcoAcceso extends JFrame implements KeyListener, ActionListener {
 	private JButton jbEnter;
 	private JTextField jtfUser;
 	private JPasswordField jpfPass;
@@ -56,20 +54,20 @@ class MarcoAcceso extends JFrame implements KeyListener, ActionListener{
 	private JTextPane jtpMessage;
 	private String alet = "Credenciales no correctas. Reint�ntalo.";
 	private boolean acceso;
-	
+
 	private Toolkit mipantalla = Toolkit.getDefaultToolkit();
 //	private Image miIcono = mipantalla.getImage("src/img/iconBank.png");
 
-	//---------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------
 //	private URL ruta_externa = MarcoAcceso.class.getResource("iconBank.png");
 //	private Image miIcono = mipantalla.getImage(ruta_externa);
-	//---------------------------------------------------------------------------------
-	
+	// ---------------------------------------------------------------------------------
+
 	private Dimension tamanoPantalla = mipantalla.getScreenSize();
 	private int alturaPantalla = tamanoPantalla.height;
-	private int anchoPantalla = tamanoPantalla.width ;
+	private int anchoPantalla = tamanoPantalla.width;
 	private int admin = 0;
-	
+
 	private InterfazAdministrador GUI_Administrador;
 	private InterfazDeUsuario<?> GUI_Cliente;
 
@@ -81,44 +79,44 @@ class MarcoAcceso extends JFrame implements KeyListener, ActionListener{
 	private String contrasena;
 	private String tipoCuenta;
 	private Connection con;
-	private  PreparedStatement p;
+	private PreparedStatement p;
 	private ResultSet rs;
-	public MarcoAcceso(String title){
-		 con = null;
-         p = null;
-         rs = null;
-    
+
+	public MarcoAcceso(String title) {
+		con = null;
+		p = null;
+		rs = null;
+
 		setTitle(title);
 
-		//setBounds(300,150,260,300);
-		setBounds(alturaPantalla/2,anchoPantalla/4,245,295);
+		// setBounds(300,150,260,300);
+		setBounds(alturaPantalla / 2, anchoPantalla / 4, 245, 295);
 //		setIconImage(miIcono);		
-		     
+
 		setResizable(false);
 		setLayout(null);
-		
+
 		jlUser = new JLabel("Usuario");
 		jtfUser = new JTextField(10);
 		jtfUser.addKeyListener(this);
-		
+
 		jlPass = new JLabel("Contrase�a");
 		jpfPass = new JPasswordField(10);
 		jpfPass.addKeyListener(this);
 		jtpMessage = new JTextPane();
-		
+
 		jtpMessage.setEditable(false);
 		jtpMessage.setBackground(getBackground());
-		
-		
+
 		jbEnter = new JButton("Acceder");
 		jbEnter.addActionListener(this);
-		
+
 		add(jlUser).setBounds(100, 25, 100, 20);
 		add(jtfUser).setBounds(60, 50, 130, 25);
 		add(jlPass).setBounds(90, 85, 100, 20);
 		add(jpfPass).setBounds(60, 110, 130, 25);
 		add(jbEnter).setBounds(75, 160, 100, 40);
-		
+
 //		add(new LaminaConImagen()).setBounds(0, 0, 245, 295);
 //		this.setBackground(new Color(245,136,8));
 		setVisible(true);
@@ -127,51 +125,50 @@ class MarcoAcceso extends JFrame implements KeyListener, ActionListener{
 	/**
 	 * @return si existe acceso de usuario o no existe
 	 */
-	public boolean getAcceso(){
+	public boolean getAcceso() {
 		return acceso;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	private boolean checkUser(){
+	private boolean checkUser() {
 		boolean checkU = false;
 		String user = jtfUser.getText();
 		char[] pass = jpfPass.getPassword();
 		String passConv = new String(pass);
-		ClientesDB []listaNueva = null;
-		
-		try{
-		    String sql = "Select users,password from USUARIOS where users = ? and password= ?";
-		    con = DBConfig.connectDB();
-		    PreparedStatement stmt = con.prepareStatement(sql);
-		    stmt.setString( 1, user );
-		    stmt.setString( 2, passConv );
-		    ResultSet rs=stmt.executeQuery();
-		    
-		    if (rs.next()) // userid found
-		    {
-		    	admin = 1;
-		    	checkU = true;
-		    }
-		    else  // user not found
-		    {
-		        System.out.println("Invalid UserId");
-		    }
-		    stmt.close();		
-			
-			
-		}catch(SQLException e)
+		ClientesDB[] listaNueva = null;
+
+		try {
+			String sql = "Select * from USUARIOS where users = ? and password= ?";
+			con = DBConfig.connectDB();
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, user);
+			stmt.setString(2, passConv);
+			ResultSet rs = stmt.executeQuery();
+
+			if (rs.next()) // userid found
 			{
-			    System.out.println(e);
+				checkU=true;
+				String rol = rs.getString(4);
+				if (rol.equals("admin")) {
+					admin = 1;
+				}
+			} else // user not found
+			{
+				System.out.println("Invalid UserId");
 			}
+			stmt.close();
 
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
 
-		if(checkU){
+		if (checkU) {
 			add(jtpMessage).setBounds(60, 140, 130, 10);
 			jtpMessage.setText(null);
 			jtpMessage.setBackground(getBackground());
 			add(jbEnter).setBounds(75, 160, 100, 40);
-			
-		}else{			
+
+		} else {
 			add(jtpMessage).setBounds(60, 140, 130, 40);
 			jtpMessage.setText(alet);
 			jtpMessage.setBackground(getBackground());
@@ -179,48 +176,50 @@ class MarcoAcceso extends JFrame implements KeyListener, ActionListener{
 			add(jbEnter).setBounds(75, 200, 100, 40);
 		}
 		return checkU;
-		
-	}	
-	
+
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub	
-		
-		if(jbEnter == e.getSource()){
+		// TODO Auto-generated method stub
+
+		if (jbEnter == e.getSource()) {
 			acceso = checkUser();
-			if(acceso & admin == 1){
+			if (acceso & admin == 1) {
 				this.dispose();
 				GUI_Administrador = new InterfazAdministrador();
 				GUI_Administrador.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				
+
 				GUI_Administrador.setVisible(true);
-			}else if(acceso){
+			} else if (acceso) {
 				this.dispose();
-				GUI_Cliente = new InterfazDeUsuario<Object>(nombre,apellido,usuario,numCuenta,saldoInicial,contrasena,tipoCuenta);
+				GUI_Cliente = new InterfazDeUsuario<Object>(nombre, apellido, usuario, numCuenta, saldoInicial,
+						contrasena, tipoCuenta);
 				GUI_Cliente.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				
+
 				GUI_Cliente.setVisible(true);
 			}
-		}	
-		
+		}
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getKeyCode() == 10){
+		if (e.getKeyCode() == 10) {
 			acceso = checkUser();
-			if(acceso & admin == 1){
+			if (acceso & admin == 1) {
 				this.dispose();
 				GUI_Administrador = new InterfazAdministrador();
 				GUI_Administrador.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				
+
 				GUI_Administrador.setVisible(true);
-			}else if(acceso){
+			} else if (acceso) {
 				this.dispose();
-				GUI_Cliente = new InterfazDeUsuario<Object>(nombre,apellido,usuario,numCuenta,saldoInicial,contrasena,tipoCuenta);
+				GUI_Cliente = new InterfazDeUsuario<Object>(nombre, apellido, usuario, numCuenta, saldoInicial,
+						contrasena, tipoCuenta);
 				GUI_Cliente.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				
+
 				GUI_Cliente.setVisible(true);
 			}
 		}
@@ -234,39 +233,35 @@ class MarcoAcceso extends JFrame implements KeyListener, ActionListener{
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-	}	
-	
-	class LaminaConImagen extends JPanel{
+	}
+
+	class LaminaConImagen extends JPanel {
 		private Image imagen;
 		URL ruta_externa = MarcoAcceso.LaminaConImagen.class.getResource("dise�oLogin5.png");
 
-		public LaminaConImagen(){
+		public LaminaConImagen() {
 //			URL ru = MarcoAcceso.class.getResource("dise�oLogin5.png");
 //			File mimagen = new File("src/img/dise�oLogin5.png");
-			
-			try{
+
+			try {
 				imagen = ImageIO.read(ruta_externa);
-			
 
 //				imagen = ImageIO.read(ru);
 //				imagen = ImageIO.read(mimagen);
-			}
-			catch(IOException e){
+			} catch (IOException e) {
 //				System.out.println("La imagen no se en cuentra");
 			}
 		}
-		public void paintComponent(Graphics g){
+
+		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			
+
 			imagen.getWidth(this);
 			imagen.getHeight(this);
-			g.drawImage(imagen,0,0, null);
+			g.drawImage(imagen, 0, 0, null);
 
 		}
-		
+
 	}
 
-
 }
-
-
